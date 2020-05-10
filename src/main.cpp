16fdafe53bfc43b0f896a2f5a93e6897d7c4d38a
Ivan Giuliani <giuliani.v@gmail.com>
2020-05-10 20:41:29 +0100

    Better management of the brightness level

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@
 
 #define NUM_LEDS 60
 #define DATA_PIN 6
-#define COLOR_ORDER GRB
 
-static const uint8_t brightness = 100;
+#define LED_BRIGHTNESS_STEP_MULTIPLIER 5
+
 CRGB leds[NUM_LEDS];
-
 RotaryEncoder encoder(D1, D2);
 ButtonCtrl encoder_button(D3);
 
@@ -23,7 +22,7 @@ void setup() {
   while(!Serial) { }
 
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(brightness);
+  FastLED.setBrightness(0);
 
   encoder.begin();
   encoder_button.begin(true);
@@ -42,21 +41,30 @@ inline void fill_leds(uint8_t hue) {
   }
 }
 
+static int16_t brightness = 0;
+void inc_brightness(int8_t brightness_offset) {
+  // Technically brightness is measured 0-255 and a uint8_t would
+  // be enough. However we still use a int16_t as to avoid looping
+  // within the range (e.g. jump from 255 to 0);
+  brightness += brightness_offset;
+  brightness = std::max(std::min(brightness, (int16_t)255), (int16_t)0);
+
+  FastLED.setBrightness(brightness);
+
+  Serial.print("brightness: ");
+  Serial.println(brightness);
+}
+
 void loop() {
-  static uint8_t brightness = 0;
   static uint8_t color_idx = 0;
 
   CRGB::HTMLColorCode colors[] = {
     CRGB::White, CRGB::Red, CRGB::Green, CRGB::Blue
   };
 
-  FastLED.setBrightness(brightness);
-
   int8_t offset = encoder.read_offset();
   if (offset != 0) {
-    brightness += offset * 3;
-    Serial.print("brightness: ");
-    Serial.println(brightness);
+    inc_brightness(offset * LED_BRIGHTNESS_STEP_MULTIPLIER);
   }
 
   for(uint8_t i = 0; i < NUM_LEDS; i++) {
